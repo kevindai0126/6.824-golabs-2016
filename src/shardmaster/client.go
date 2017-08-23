0059ad6c922c106297970d542016bc4621e6d06d
yundai <yundai@paypal.com>
2017-08-23 22:49:26 +0800

    Finish lab4 part A

--- src/shardmaster/client.go
+++ src/shardmaster/client.go
@@ -7,11 +7,17 @@ package shardmaster
 import "labrpc"
 import "time"
 import "crypto/rand"
-import "math/big"
+import (
+	"math/big"
+	"sync"
+)
 
 type Clerk struct {
 	servers []*labrpc.ClientEnd
 	// Your data here.
+	id int64
+	serNum int
+	mu      sync.Mutex
 }
 
 func nrand() int64 {
@@ -25,6 +31,8 @@ func MakeClerk(servers []*labrpc.ClientEnd) *Clerk {
 	ck := new(Clerk)
 	ck.servers = servers
 	// Your code here.
+	ck.id =nrand()
+	ck.serNum = 0
 	return ck
 }
 
@@ -32,6 +40,12 @@ func (ck *Clerk) Query(num int) Config {
 	args := &QueryArgs{}
 	// Your code here.
 	args.Num = num
+	args.ClientId = ck.id
+	ck.mu.Lock()
+	args.SerNum = ck.serNum
+	ck.serNum++
+	ck.mu.Unlock()
+
 	for {
 		// try each known server.
 		for _, srv := range ck.servers {
@@ -49,6 +63,11 @@ func (ck *Clerk) Join(servers map[int][]string) {
 	args := &JoinArgs{}
 	// Your code here.
 	args.Servers = servers
+	args.ClientId = ck.id
+	ck.mu.Lock()
+	args.SerNum = ck.serNum
+	ck.serNum++
+	ck.mu.Unlock()
 
 	for {
 		// try each known server.
@@ -67,6 +86,11 @@ func (ck *Clerk) Leave(gids []int) {
 	args := &LeaveArgs{}
 	// Your code here.
 	args.GIDs = gids
+	args.ClientId = ck.id
+	ck.mu.Lock()
+	args.SerNum = ck.serNum
+	ck.serNum++
+	ck.mu.Unlock()
 
 	for {
 		// try each known server.
@@ -86,6 +110,11 @@ func (ck *Clerk) Move(shard int, gid int) {
 	// Your code here.
 	args.Shard = shard
 	args.GID = gid
+	args.ClientId = ck.id
+	ck.mu.Lock()
+	args.SerNum = ck.serNum
+	ck.serNum++
+	ck.mu.Unlock()
 
 	for {
 		// try each known server.
--- src/shardmaster/server.go
+++ src/shardmaster/server.go
@@ -4,7 +4,11 @@ package shardmaster
 import "raft"
 import "labrpc"
 import "sync"
-import "encoding/gob"
+import (
+	"encoding/gob"
+	"time"
+	"fmt"
+)
 
 
 type ShardMaster struct {
@@ -14,32 +18,284 @@ type ShardMaster struct {
 	applyCh chan raft.ApplyMsg
 
 	// Your data here.
-
 	configs []Config // indexed by config num
+	result	map[int]chan Op
+	ack 	map[int64]int
 }
 
 
 type Op struct {
 	// Your data here.
+	Operator      string
+	Num     int
+	GIDs    []int
+	Servers map[int][]string
+	Shard   int
+	ClientId int64
+	SerNum int
 }
 
+func (sm *ShardMaster) isDedup(clientId int64, serNum int) bool {
+	sm.mu.Lock()
+	latestNum, ok := sm.ack[clientId]
+	sm.mu.Unlock()
+
+	if(ok && serNum <= latestNum) {
+		return true
+	} else {
+		return false
+	}
+}
+
+func (sm *ShardMaster) ReplicateLog(entry Op) bool {
+	index, _, isLeader := sm.rf.Start(entry)
+
+
+	if (!isLeader) {
+		return false
+	} else {
+		sm.mu.Lock()
+		ch, ok := sm.result[index]
+		if (!ok) {
+			ch = make(chan Op, 1)
+			sm.result[index] = ch
+		}
+		sm.mu.Unlock()
+
+		select {
+		case op := <-ch:
+			return op.SerNum == entry.SerNum
+		case <-time.After(1000 * time.Millisecond):
+		//log.Printf("timeout\n")
+			return false
+		}
+	}
+}
+
+func (sm *ShardMaster) getLatestConfig() Config {
+	return sm.configs[len(sm.configs) - 1]
+}
+
+func (sm *ShardMaster) getLatestConfigReference() *Config {
+	return &sm.configs[len(sm.configs) - 1]
+}
 
 func (sm *ShardMaster) Join(args *JoinArgs, reply *JoinReply) {
 	// Your code here.
+	if(sm.isDedup(args.ClientId, args.SerNum)) {
+		reply.WrongLeader = false
+		reply.Err = OK
+	} else {
+
+		entry := Op{Operator:"Join", Servers:args.Servers, ClientId:args.ClientId, SerNum:args.SerNum}
+
+		ok := sm.ReplicateLog(entry)
+
+		if ok {
+			reply.WrongLeader = false
+			reply.Err = OK
+		} else {
+			reply.WrongLeader = true
+		}
+	}
 }
 
 func (sm *ShardMaster) Leave(args *LeaveArgs, reply *LeaveReply) {
 	// Your code here.
+	if(sm.isDedup(args.ClientId, args.SerNum)) {
+		reply.WrongLeader = false
+		reply.Err = OK
+	} else {
+
+		entry := Op{Operator:"Leave", GIDs:args.GIDs, ClientId:args.ClientId, SerNum:args.SerNum}
+
+		ok := sm.ReplicateLog(entry)
+
+		if ok {
+			reply.WrongLeader = false
+			reply.Err = OK
+		} else {
+			reply.WrongLeader = true
+		}
+	}
 }
 
 func (sm *ShardMaster) Move(args *MoveArgs, reply *MoveReply) {
 	// Your code here.
+	if(sm.isDedup(args.ClientId, args.SerNum)) {
+		reply.WrongLeader = false
+		reply.Err = OK
+	} else {
+
+		entry := Op{Operator:"Move", Shard:args.Shard, GIDs: append(make([]int, 0),args.GID), ClientId:args.ClientId, SerNum:args.SerNum}
+
+		ok := sm.ReplicateLog(entry)
+
+		if ok {
+			reply.WrongLeader = false
+			reply.Err = OK
+		} else {
+			reply.WrongLeader = true
+		}
+	}
 }
 
 func (sm *ShardMaster) Query(args *QueryArgs, reply *QueryReply) {
 	// Your code here.
+	if(sm.isDedup(args.ClientId, args.SerNum)) {
+		if(args.Num == -1 || args.Num >= len(sm.configs)) {
+			reply.Config = sm.getLatestConfig()
+		} else {
+			reply.Config = sm.configs[args.Num]
+		}
+	} else {
+
+		entry := Op{Operator:"Query", Num:args.Num, ClientId:args.ClientId, SerNum:args.SerNum}
+
+		ok := sm.ReplicateLog(entry)
+
+		if ok {
+			reply.WrongLeader = false
+			reply.Err = OK
+			sm.mu.Lock()
+			if(args.Num == -1 || args.Num >= len(sm.configs)) {
+				reply.Config = sm.getLatestConfig()
+			} else {
+				reply.Config = sm.configs[args.Num]
+			}
+			sm.ack[args.ClientId] = args.SerNum
+			sm.mu.Unlock()
+		} else {
+			reply.WrongLeader = true
+		}
+	}
 }
 
+func (sm *ShardMaster) newConfig() *Config {
+	old := &sm.configs[len(sm.configs) - 1]
+	new := Config{}
+	new.Groups = map[int][]string{}
+	new.Num = old.Num + 1
+	new.Shards = [NShards]int{}
+	for gid, servers := range old.Groups {
+		new.Groups[gid] = servers
+	}
+	for i, gid := range old.Shards {
+		new.Shards[i] = gid
+	}
+	sm.configs = append(sm.configs, new)
+	return &sm.configs[len(sm.configs) - 1]
+
+}
+
+func (sm *ShardMaster) showConfig(config Config) {
+	println("Server", sm.me)
+	println("Num", config.Num)
+	println("Groups")
+	for gid, servers := range config.Groups {
+		println("GID", gid)
+		fmt.Printf("%v\n", servers)
+	}
+	fmt.Printf("Shards:%v]n", config.Shards)
+}
+
+func (sm *ShardMaster) getOneShardByGID(gid int) int {
+	config := sm.getLatestConfigReference()
+
+	for shard, id := range config.Shards {
+		if id == gid {
+			return shard
+		}
+	}
+
+	return -1
+}
+
+func (sm *ShardMaster) getGIDWithMostShard() int {
+	config := sm.getLatestConfigReference()
+
+	//There is unassigned shard
+	for _, gid := range config.Shards {
+		if gid == 0 {
+			return 0
+		}
+	}
+
+	count := map[int]int{}
+	max := -1
+	result := 0
+
+	for gid := range config.Groups {
+		count[gid] = 0
+	}
+
+	for _, gid := range config.Shards {
+		count[gid]++
+	}
+
+	for gid, c := range count {
+		_, ok := config.Groups[gid]
+		if ok && c > max {
+			max, result = c, gid
+		}
+	}
+
+	return result
+}
+
+func (sm *ShardMaster) getGIDWithLeastShard() int {
+	config := sm.getLatestConfigReference()
+
+	//There is unassigned shard
+	for _, gid := range config.Shards {
+		if gid == 0 {
+			return 0
+		}
+	}
+
+	count := map[int]int{}
+	min := len(config.Shards)
+	result := 0
+
+	for gid := range config.Groups {
+		count[gid] = 0
+	}
+
+	for _, gid := range config.Shards {
+		count[gid]++
+	}
+
+	for gid, c := range count {
+		_, ok := config.Groups[gid]
+		if ok && c < min {
+			min, result = c, gid
+		}
+	}
+
+	return result
+}
+
+func (sm *ShardMaster) rebalanceAfterJoin(gid int) {
+	config := sm.getLatestConfigReference()
+	num := NShards / len(config.Groups)
+
+	for i := 0; i < num; i++ {
+		maxGid := sm.getGIDWithMostShard()
+		shard := sm.getOneShardByGID(maxGid)
+		config.Shards[shard] = gid
+	}
+}
+
+func (sm *ShardMaster) rebalanceAfterLeave(gid int) {
+	config := sm.getLatestConfigReference()
+
+	for shard, id := range config.Shards {
+		if (id == gid) {
+			minGid := sm.getGIDWithLeastShard()
+			config.Shards[shard] = minGid
+		}
+	}
+}
 
 //
 // the tester calls Kill() when a ShardMaster instance won't
@@ -73,8 +329,66 @@ func StartServer(servers []*labrpc.ClientEnd, me int, persister *raft.Persister)
 	gob.Register(Op{})
 	sm.applyCh = make(chan raft.ApplyMsg)
 	sm.rf = raft.Make(servers, me, persister, sm.applyCh)
+	sm.result = make(map[int]chan Op)
+	sm.ack = make(map[int64]int)
 
 	// Your code here.
+	go func() {
+		for {
+			msg := <-sm.applyCh
+			if msg.UseSnapshot {
+			} else {
+				op := msg.Command.(Op)
+
+				if(!sm.isDedup(op.ClientId, op.SerNum)) {
+					sm.mu.Lock()
+					switch op.Operator {
+					case "Join":
+						config := sm.newConfig()
+						for gid, servers := range op.Servers {
+							_, ok := config.Groups[gid]
+							if !ok {
+								config.Groups[gid] = servers
+								sm.rebalanceAfterJoin(gid)
+							}
+						}
+
+						//sm.showConfig(sm.getLatestConfig())
+					case "Leave":
+						config := sm.newConfig()
+						for _, gid := range op.GIDs {
+							_, ok := config.Groups[gid]
+							if ok {
+								delete(config.Groups, gid)
+								sm.rebalanceAfterLeave(gid)
+							}
+						}
+					case "Move":
+						config := sm.newConfig()
+						if op.GIDs != nil && len(op.GIDs) > 0 {
+							config.Shards[op.Shard] = op.GIDs[0]
+						}
+					}
+					sm.ack[op.ClientId] = op.SerNum
+					sm.mu.Unlock()
+				}
+				sm.mu.Lock()
+
+				ch, ok := sm.result[msg.Index]
+				if !ok {
+					sm.result[msg.Index] = make(chan Op, 1)
+				} else {
+					select {
+					case <-ch:
+					default:
+					}
+					ch <- op
+				}
+
+				sm.mu.Unlock()
+			}
+		}
+	}()
 
 	return sm
 }
